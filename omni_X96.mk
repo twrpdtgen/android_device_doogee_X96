fe8e897cddbe83ce159d98f64c181099b772b849
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-10-15 20:12:36 +0200

    X96: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm Manufacturer: doogee Model: X96 Script version: 3.0.0

---
+++ omni_X96.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from X96 device
+$(call inherit-product, device/doogee/X96/device.mk)
+
+PRODUCT_DEVICE := X96
+PRODUCT_NAME := omni_X96
+PRODUCT_BRAND := DOOGEE
+PRODUCT_MODEL := X96
+PRODUCT_MANUFACTURER := doogee
+
+PRODUCT_GMS_CLIENTID_BASE := android-doogee
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="DOOGEE-X96_EEA_20220114_V13"
+
+BUILD_FINGERPRINT := DOOGEE/X96_EEA/X96:11/RP1A.201005.001/1376:user/release-keys

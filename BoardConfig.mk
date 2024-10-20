d9c70c1c38471ee38d1f7968210e88aa05255854
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2024-10-20 17:28:11 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,9 +1,4 @@
-#
 # Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
 
 DEVICE_PATH := device/doogee/X96
 

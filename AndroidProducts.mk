6331e21ea174c783482bfaaae6ef19a97b8c9d95
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2024-10-20 17:27:16 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,6 @@
-#
 # Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_X96.mk
 
-COMMON_LUNCH_CHOICES := \
-    twrp_X96-user \
-    twrp_X96-userdebug \
-    twrp_X96-eng
+COMMON_LUNCH_CHOICES := twrp_X96-eng

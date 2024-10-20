1032a4c59b880a475261c8d9a2bd96e4f054d65d
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2024-10-20 17:28:38 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,9 +1,5 @@
-#
 # Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
+
 LOCAL_PATH := device/doogee/X96
 
 PRODUCT_PLATFORM := sp9863a
